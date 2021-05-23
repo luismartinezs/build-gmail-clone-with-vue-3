5807738d15ac7dac829d32f5ea7f052917bf32d7
luismartinezs <martinez.s.luis@gmail.com>
2021-05-23 16:48:23 +0200

    opening an email

--- src/components/MailTable.vue
+++ src/components/MailTable.vue
@@ -5,7 +5,7 @@
         v-for="email in unarchivedEmails"
         :key="email.id"
         :class="['clickable', email.read ? 'read' : '']"
-        @click="readEmail(email)"
+        @click="openEmail(email)"
       >
         <td>
           <input type="checkbox" />
@@ -24,20 +24,26 @@
       </tr>
     </tbody>
   </table>
+  <MailView v-if="openedEmail" :email="openedEmail" />
 </template>
 
 <script>
 import { format } from "date-fns";
 import { ref } from "vue";
 import axios from "axios";
+import MailView from "@/components/MailView.vue";
 
 export default {
   name: "MailTable",
+  components: {
+    MailView,
+  },
   async setup() {
     let { data: emails } = await axios.get("http://localhost:3000/emails");
     return {
       format,
       emails: ref(emails),
+      openedEmail: ref(null),
     };
   },
   computed: {
@@ -51,9 +57,10 @@ export default {
     },
   },
   methods: {
-    readEmail(email) {
+    openEmail(email) {
       email.read = true;
       this.updateEmail(email);
+      this.openedEmail = email;
     },
     archiveEmail(email) {
       email.archived = true;
